8e808672dc17d37ae062f3ecd9a4ee8eb612904b
Gonzalo Coelho <coelhogonza@gmail.com>
2022-01-16 10:27:56 -0300

    Used template for home

--- generator/views.py
+++ generator/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'generator/home.html')
 
 def eggs(request):
     return HttpResponse('Eggs page')
