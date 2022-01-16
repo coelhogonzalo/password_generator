d6c99e7aad61a483720abdfdfa99577448918060
Gonzalo Coelho <coelhogonza@gmail.com>
2022-01-16 15:55:44 -0300

    Implemented password generator template

--- generator/views.py
+++ generator/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import string    
+import random 
+
 # Create your views here.
 
 def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request, 'generator/password.html')
+    uppercase = request.GET.get('uppercase', False)
+    numbers = request.GET.get('numbers', False)
+    length = int(request.GET.get('length', 12))
+    choices = string.ascii_lowercase
+    if uppercase:
+        choices = choices + string.ascii_uppercase
+    if numbers:
+        choices = choices + string.digits
+    userPassword = ''.join(random.choices(choices, k = length))    
+   
+    return render(request, 'generator/password.html', {'password': userPassword})
